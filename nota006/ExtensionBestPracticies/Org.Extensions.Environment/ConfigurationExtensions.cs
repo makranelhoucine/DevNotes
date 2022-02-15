d7368370c2cd544db3c48429dba3eadca9801adc
informatropico <informatropico@gmail.com>
2022-02-15 23:21:38 +0100

    extension

--- nota006/ExtensionBestPracticies/Org.Extensions.Environment/ConfigurationExtensions.cs
+++ nota006/ExtensionBestPracticies/Org.Extensions.Environment/ConfigurationExtensions.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
-using Microsoft.Extensions.Configuration.EnvironmentVariables;
 
 namespace Org.Extensions.Environment
 {
