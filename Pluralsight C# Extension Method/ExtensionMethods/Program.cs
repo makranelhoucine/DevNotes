38c3a5f17d5ecb186135c2b6ebf309f551f05bcb
informatropico <informatropico@gmail.com>
2022-02-16 00:01:18 +0100

    riorganizzato directory

---
+++ Pluralsight C# Extension Method/ExtensionMethods/Program.cs
@@ -0,0 +1,11 @@
+﻿using ExtensionMethods.Extensions;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder();
+var config = builder.AddStandardProviders().Build();
+System.Console.WriteLine(config.Providers.Count());
+System.Console.WriteLine(config.IsLoaded());
+
+var target = new Target("id1");
+System.Console.WriteLine(target.GetStandardizedId());
+
